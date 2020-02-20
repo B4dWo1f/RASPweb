6d0f986c0280a4ecaeaa49dbb813de2dfebbee08
n03l <B4dWo1f@protonmail.com>
2020-02-20 18:08:30 +0100

    alpha for language translation

--- js/initialize.js
+++ js/initialize.js
@@ -83,3 +83,32 @@ CB_R_layer.src = folder+'/'+'rain1.png';
 document.getElementById("days").innerHTML = generate_days();
 
 document.getElementById("hours").innerHTML = generate_hours();
+
+var translations = {
+   en: {
+      sfcwind : 'SFCwind',
+      blwind : 'BLwind',
+      bltopwind : 'BLTOPwind',
+      hgldier: 'Max. Therm. Height',
+      zsfclcl: 'Cu Cloudbase @CuPot>0',
+      cape: 'CAPE',
+      wstar: 'Thermal Updraft Velocity',
+      wblmaxmin: 'BL Max.Up/Down Motion',
+      zblcl: 'OD Cloudbase @ODpot>0',
+      rain: '%Clouds & rain',
+      press: 'Isobars'
+   },
+   es: {
+      sfcwind : 'Superficie',
+      blwind : 'Promedio',
+      bltopwind : 'Techo',
+      hgldier: 'Techo (Azul)',
+      zsfclcl: 'Base cúmulos',
+      cape: 'CAPE',
+      wstar: 'Térmica',
+      wblmaxmin: 'Convergencia',
+      zblcl: 'Base cobertura (8/8)',
+      rain: 'Nubes y lluvia',
+      press: 'Isobaras'
+   }
+};
